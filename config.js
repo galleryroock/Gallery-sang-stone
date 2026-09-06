a2ab3f45eea60f1f1a7ab4f5494686a22cc15535
galleryroock <galleryroock@gmail.com>
2026-09-06 13:18:57 +0330

    Update Supabase configuration variable names

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.https://svbaymqpozkhwytzsahq.supabase.co/rest/v1/
-window.sb_publishable_lJMC5SxyJDTkwP12bM02kQ_HkqtqDK8
+window.SUPABASE_URL =https://svbaymqpozkhwytzsahq.supabase.co/rest/v1/
+window.SUPABASE_KEY =sb_publishable_lJMC5SxyJDTkwP12bM02kQ_HkqtqDK8
